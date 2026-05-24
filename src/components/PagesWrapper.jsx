c151ef8c483954d5e8ecea4e0f6c2515d50cad4d
Imkrishna67 <mysystematwork@gmail.com>
2026-05-24 15:35:28 +0530

    fix navbar rendering

--- src/components/PagesWrapper.jsx
+++ src/components/PagesWrapper.jsx
@@ -1,7 +1,17 @@
+import Navbar from './Navbar'
+
 export default function PagesWrapper({ children }) {
-  return <div className="pages-wrapper">{children}</div>
+  return (
+    <div className="pages-wrapper">
+      {children}
+    </div>
+  )
 }
 
 export function Header() {
-  return <header className="app-header"></header>
+  return (
+    <header className="app-header">
+      <Navbar />
+    </header>
+  )
 }
